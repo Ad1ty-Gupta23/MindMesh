4d4b6000d02928de6c1d4f45bf087a24a3f76e7e
Aditya <gupta.aditya2309@gmail.com>
2025-06-14 16:37:22 +0530

    entry

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Login from './pages/login'
+import Register from './pages/register'
+import Home from './pages/home'
+import HabitBlockchainProvider from './context/HabitBlockchainContext'
+function App() {
+ 
+
+  return (
+    <HabitBlockchainProvider>
+   
+    <BrowserRouter>
+      <Routes>
+         <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        </Routes>
+    </BrowserRouter>
+    </HabitBlockchainProvider>
+  )
+}
+
+export default App
