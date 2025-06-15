af4763086a1ddc86b278ab0e8d9731ad7f1998aa
Aditya <gupta.aditya2309@gmail.com>
2025-06-15 15:07:26 +0530

    g

--- backend/aimirror.py
+++ backend/aimirror.py
@@ -286,5 +286,5 @@ async def get_ai_feedback(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8003)
-    #uvicorn aimirror:app --reload --port 8003
+    uvicorn.run(app, host="0.0.0.0", port=8002)
+    #uvicorn aimirror:app --reload --port 8002
